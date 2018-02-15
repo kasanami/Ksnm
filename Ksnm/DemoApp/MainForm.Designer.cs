12cba30c06702ff62c0a8977605d182a218fc106
笠浪貴大 <dev.kacha@gmail.com>
2018-02-15 16:24:14 +0900

    Randomデモ実装

--- Ksnm/DemoApp/MainForm.Designer.cs
+++ Ksnm/DemoApp/MainForm.Designer.cs
@@ -28,22 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Random_PictureBox = new System.Windows.Forms.PictureBox();
+            this.Random_GenerateButton1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.Random_TypeComboBox = new System.Windows.Forms.ComboBox();
+            this.Random_SeedNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Random_PictureBox)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Random_SeedNumericUpDown)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(747, 489);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(739, 463);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Math";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.tableLayoutPanel1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(739, 463);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Random";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.Random_PictureBox, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(733, 457);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Random_PictureBox
+            // 
+            this.Random_PictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Random_PictureBox.Location = new System.Drawing.Point(23, 103);
+            this.Random_PictureBox.Name = "Random_PictureBox";
+            this.Random_PictureBox.Size = new System.Drawing.Size(687, 331);
+            this.Random_PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Random_PictureBox.TabIndex = 0;
+            this.Random_PictureBox.TabStop = false;
+            // 
+            // Random_GenerateButton1
+            // 
+            this.Random_GenerateButton1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Random_GenerateButton1.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Random_GenerateButton1.Location = new System.Drawing.Point(459, 3);
+            this.Random_GenerateButton1.Name = "Random_GenerateButton1";
+            this.Random_GenerateButton1.Size = new System.Drawing.Size(225, 38);
+            this.Random_GenerateButton1.TabIndex = 1;
+            this.Random_GenerateButton1.Text = "乱数生成";
+            this.Random_GenerateButton1.UseVisualStyleBackColor = true;
+            this.Random_GenerateButton1.Click += new System.EventHandler(this.Random_GenerateButton1_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Controls.Add(this.Random_GenerateButton1, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.Random_TypeComboBox, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.Random_SeedNumericUpDown, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(23, 53);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(687, 44);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // Random_TypeComboBox
+            // 
+            this.Random_TypeComboBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Random_TypeComboBox.FormattingEnabled = true;
+            this.Random_TypeComboBox.Location = new System.Drawing.Point(3, 3);
+            this.Random_TypeComboBox.Name = "Random_TypeComboBox";
+            this.Random_TypeComboBox.Size = new System.Drawing.Size(222, 20);
+            this.Random_TypeComboBox.TabIndex = 2;
+            // 
+            // Random_SeedNumericUpDown
+            // 
+            this.Random_SeedNumericUpDown.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Random_SeedNumericUpDown.Location = new System.Drawing.Point(231, 3);
+            this.Random_SeedNumericUpDown.Name = "Random_SeedNumericUpDown";
+            this.Random_SeedNumericUpDown.Size = new System.Drawing.Size(222, 19);
+            this.Random_SeedNumericUpDown.TabIndex = 3;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(747, 489);
+            this.Controls.Add(this.tabControl1);
             this.Name = "MainForm";
             this.Text = "Ksnm Demo";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Random_PictureBox)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Random_SeedNumericUpDown)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox Random_PictureBox;
+        private System.Windows.Forms.Button Random_GenerateButton1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.ComboBox Random_TypeComboBox;
+        private System.Windows.Forms.NumericUpDown Random_SeedNumericUpDown;
     }
 }
 
--- Ksnm/DemoApp/MainForm.cs
+++ Ksnm/DemoApp/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ksnm.Randoms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -22,6 +23,8 @@ namespace DemoApp
             // ウィンドウの位置・サイズを復元
             Bounds = Properties.Settings.Default.Bounds;
             WindowState = Properties.Settings.Default.WindowState;
+
+            OnLoad_RandomTabPage();
         }
 
         /// <summary>
@@ -49,5 +52,65 @@ namespace DemoApp
                 Properties.Settings.Default.Save();
             }
         }
+
+        #region Randomタブ
+
+        Random random = new Random();
+        Xorshift128 xorshift128 = new Xorshift128();
+        IncrementRandom incrementRandom = new IncrementRandom();
+
+        private void OnLoad_RandomTabPage()
+        {
+            // 描画先とするImageオブジェクトを作成
+            var canvas = new Bitmap(500, 500);
+            Random_PictureBox.Image = canvas;
+            using (var graphics = Graphics.FromImage(canvas))
+            {
+                graphics.Clear(Color.White);
+            }
+
+            Random_TypeComboBox.Items.Add("Random");
+            Random_TypeComboBox.Items.Add("Xorshift128");
+            Random_TypeComboBox.Items.Add("IncrementRandom");
+            Random_TypeComboBox.SelectedIndex = 0;
+        }
+
+        private void Random_GenerateButton1_Click(object sender, EventArgs e)
+        {
+            Random random = null;
+            if (Random_TypeComboBox.Text == "Random")
+            {
+                random = this.random;
+            }
+            else if (Random_TypeComboBox.Text == "Xorshift128")
+            {
+                random = xorshift128;
+            }
+            else if (Random_TypeComboBox.Text == "IncrementRandom")
+            {
+                random = incrementRandom;
+            }
+            else
+            {
+                return;
+            }
+
+            var pen = new Pen(Color.Black, 1);
+            var canvas = Random_PictureBox.Image;
+            int x, y;
+            using (var graphics = Graphics.FromImage(canvas))
+            {
+                graphics.Clear(Color.White);
+                for (int i = 0; i < 5000; i++)
+                {
+                    x = random.Next(canvas.Width);
+                    y = random.Next(canvas.Height);
+                    graphics.DrawRectangle(pen, x, y, 1, 1);
+                }
+            }
+            Random_PictureBox.Refresh();
+        }
+
+        #endregion Randomタブ
     }
 }
