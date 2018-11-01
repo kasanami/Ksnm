c18f5101faa6707808e8570d8a0667f0b6cfa0ee
笠浪貴大 <dev.kacha@gmail.com>
2018-11-01 20:45:45 +0900

    乱数のテストの共通処理を分離

---
+++ Ksnm/KsnmTests/Randoms/Base.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Ksnm.Randoms.Tests
+{
+    public class Base
+    {
+        /// <summary>
+        /// テスト回数
+        /// 全ての生成値をテストできないが、しょうがない。
+        /// </summary>
+        const int TestCount = 10000000;
+
+        public static void NextTest(Random random)
+        {
+            // 0以上、int.MaxValue未満か？
+            for (int i = 0; i < TestCount; i++)
+            {
+                var sample = random.Next();
+                Assert.IsTrue(sample >= 0);
+                Assert.IsTrue(sample < int.MaxValue);
+            }
+            // 0以上、2未満か？
+            for (int i = 0; i < TestCount; i++)
+            {
+                var sample = random.Next(2);
+                Assert.IsTrue(sample >= 0);
+                Assert.IsTrue(sample < 2);
+            }
+            // 1以上、3未満か？
+            for (int i = 0; i < TestCount; i++)
+            {
+                var sample = random.Next(1, 3);
+                Assert.IsTrue(sample >= 1);
+                Assert.IsTrue(sample < 3);
+            }
+        }
+
+        public static void NextDoubleTest(Random random)
+        {
+            // 0.0 以上 1.0 未満か？
+            for (int i = 0; i < TestCount; i++)
+            {
+                var sample = random.NextDouble();
+                Assert.IsTrue(sample >= 0.0);
+                Assert.IsTrue(sample < 1.0);
+            }
+        }
+    }
+}
--- Ksnm/KsnmTests/Randoms/Xorshift128Tests.cs
+++ Ksnm/KsnmTests/Randoms/Xorshift128Tests.cs
@@ -1,60 +1,23 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Ksnm.Randoms;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ksnm.Randoms.Tests
 {
     [TestClass()]
-    public class Xorshift128Tests
+    public class Xorshift128Tests : Base
     {
-        /// <summary>
-        /// テスト回数
-        /// 全ての乱数をテストできないが、しょうがない。
-        /// </summary>
-        const int TestCount = 10000000;
-
         [TestMethod()]
         public void NextTest()
         {
             Random random = new Xorshift128();
-            // 0以上、int.MaxValue未満か？
-            for (int i = 0; i < TestCount; i++)
-            {
-                var sample = random.Next();
-                Assert.IsTrue(sample >= 0);
-                Assert.IsTrue(sample < int.MaxValue);
-            }
-            // 0以上、2未満か？
-            for (int i = 0; i < TestCount; i++)
-            {
-                var sample = random.Next(2);
-                Assert.IsTrue(sample >= 0);
-                Assert.IsTrue(sample < 2);
-            }
-            // 1以上、3未満か？
-            for (int i = 0; i < TestCount; i++)
-            {
-                var sample = random.Next(1, 3);
-                Assert.IsTrue(sample >= 1);
-                Assert.IsTrue(sample < 3);
-            }
+            NextTest(random);
         }
 
         [TestMethod()]
         public void NextDoubleTest()
         {
             Random random = new Xorshift128();
-            // 0.0 以上 1.0 未満か？
-            for (int i = 0; i < TestCount; i++)
-            {
-                var sample = random.NextDouble();
-                Assert.IsTrue(sample >= 0.0);
-                Assert.IsTrue(sample < 1.0);
-            }
+            NextDoubleTest(random);
         }
     }
 }
