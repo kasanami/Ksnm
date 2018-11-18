9e7982f957d276f31d54c09108b5738efe9b678b
笠浪貴大 <dev.kacha@gmail.com>
2018-11-18 17:20:07 +0900

    RandomTest用にSample追加

---
+++ Ksnm/KsnmTests/Randoms/Sample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksnm.Randoms.Tests
+{
+    /// <summary>
+    /// テスト用乱数生成器
+    /// </summary>
+    class Sample : Ksnm.Randoms.RandomBase
+    {
+        static readonly ulong[] Values = new ulong[]
+        {
+            0,
+            1,
+            0xFFFFFFFFFFFFFFFE,
+            0xFFFFFFFFFFFFFFFF,
+        };
+        int currentIndex;
+
+        /// <summary>
+        /// 0 以上で 0xFFFFFFFF 以下の乱数を返します。
+        /// </summary>
+        /// <returns>0 以上で 0xFFFFFFFF 以下の32 ビット符号無し整数。</returns>
+        public override uint GenerateUInt32()
+        {
+            return (uint)GenerateUInt64();
+        }
+
+        /// <summary>
+        /// 0 以上で 0xFFFFFFFFFFFFFFFF 以下の乱数を返します。
+        /// </summary>
+        /// <returns>0 以上で 0xFFFFFFFFFFFFFFFF 以下の64 ビット符号無し整数。</returns>
+        public override ulong GenerateUInt64()
+        {
+            var value = Values[currentIndex];
+            currentIndex++;
+            if (currentIndex >= Values.Length)
+            {
+                currentIndex = 0;
+            }
+            return value;
+        }
+    }
+}
---
+++ Ksnm/KsnmTests/Randoms/SampleTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Ksnm.Randoms.Tests
+{
+    [TestClass()]
+    public class SampleTests : Base
+    {
+        [TestMethod()]
+        public void NextTest()
+        {
+            var random = new Sample();
+            NextTest(random);
+        }
+
+        [TestMethod()]
+        public void NextUInt32Test()
+        {
+            var random = new Sample();
+            NextUInt32Test(random);
+        }
+
+        [TestMethod()]
+        public void NextInt64Test()
+        {
+            var random = new Sample();
+            NextInt64Test(random);
+        }
+
+        [TestMethod()]
+        public void NextUInt64Test()
+        {
+            var random = new Sample();
+            NextUInt64Test(random);
+        }
+
+        [TestMethod()]
+        public void NextDoubleTest()
+        {
+            var random = new Sample();
+            NextDoubleTest(random);
+        }
+    }
+}
