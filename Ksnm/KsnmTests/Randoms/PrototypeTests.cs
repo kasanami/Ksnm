e69fcc2fe0fddb391f1c51178c99911db6ec806f
笠浪貴大 <dev.kacha@gmail.com>
2018-11-22 02:43:05 +0900

    初期値を時刻由来に変更

--- Ksnm/KsnmTests/Randoms/PrototypeTests.cs
+++ Ksnm/KsnmTests/Randoms/PrototypeTests.cs
@@ -9,35 +9,35 @@ namespace Ksnm.Randoms.Tests
         [TestMethod()]
         public void NextTest()
         {
-            var random = new Prototype(0xFFFFFFFF, 12345, 1234567);
+            var random = new Prototype();
             NextTest(random);
         }
 
         [TestMethod()]
         public void NextUInt32Test()
         {
-            var random = new Prototype(0xFFFFFFFF, 12345, 1234567);
+            var random = new Prototype();
             NextUInt32Test(random);
         }
 
         [TestMethod()]
         public void NextInt64Test()
         {
-            var random = new Prototype(0xFFFFFFFF, 12345, 1234567);
+            var random = new Prototype();
             NextInt64Test(random);
         }
 
         [TestMethod()]
         public void NextUInt64Test()
         {
-            var random = new Prototype(0xFFFFFFFF, 12345, 1234567);
+            var random = new Prototype();
             NextUInt64Test(random);
         }
 
         [TestMethod()]
         public void NextDoubleTest()
         {
-            var random = new Prototype(0xFFFFFFFF, 12345, 1234567);
+            var random = new Prototype();
             NextDoubleTest(random);
         }
     }
