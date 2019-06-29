d65146fd008452509b6ed814dabb78914b4a44d1
笠浪貴大 <dev.kacha@gmail.com>
2019-06-30 03:47:03 +0900

    ReadOnlyCollectionの拡張メソッド追加

---
+++ Ksnm/Ksnm/ExtensionMethods/System/Collections/ReadOnlyCollection.cs
@@ -0,0 +1,64 @@
+﻿/*
+The zlib License
+
+Copyright (c) 2019 Takahiro Kasanami
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source distribution.
+*/
+using System.Collections.ObjectModel;
+
+namespace Ksnm.ExtensionMethods.System.Collections
+{
+    /// <summary>
+    /// ReadOnlyCollectionの拡張メソッド
+    /// </summary>
+    public static class ReadOnlyCollection
+    {
+        /// <summary>
+        /// ランダムな位置にある要素を返します。
+        /// </summary>
+        public static T ElementAtRandom<T>(this ReadOnlyCollection<T> self, global::System.Random random)
+        {
+            var index = self.IndexOfRandom(random);
+            return self[index];
+        }
+        /// <summary>
+        /// ランダムな位置にある要素を返します。
+        /// </summary>
+        public static T ElementAtRandom<T>(this ReadOnlyCollection<T> self)
+        {
+            var index = self.IndexOfRandom();
+            return self[index];
+        }
+        /// <summary>
+        /// ランダムなindexを返します。
+        /// </summary>
+        public static int IndexOfRandom<T>(this ReadOnlyCollection<T> self, global::System.Random random)
+        {
+            return random.Next(self.Count);
+        }
+        /// <summary>
+        /// ランダムなindexを返します。
+        /// </summary>
+        public static int IndexOfRandom<T>(this ReadOnlyCollection<T> self)
+        {
+            return self.IndexOfRandom(new global::System.Random());
+        }
+    }
+}
---
+++ Ksnm/KsnmTests/ExtensionMethods/System/Collections/ReadOnlyCollectionTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ksnm.ExtensionMethods.System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksnm.ExtensionMethods.System.Collections.Tests
+{
+    [TestClass()]
+    public class ReadOnlyCollectionTests
+    {
+        [TestMethod()]
+        public void ElementAtRandomTest()
+        {
+            var sample = (new List<int>() { 0, 1, 2 }).AsReadOnly();
+            var random = new Ksnm.Randoms.IncrementRandom(0);
+            for (int i = 0; i < sample.Count * 2; i++)
+            {
+                var element = sample.ElementAtRandom(random);
+                Assert.IsTrue(sample.Contains(element));
+            }
+        }
+
+        [TestMethod()]
+        public void IndexOfRandomTest()
+        {
+            var sample = (new List<int>() { 0, 1, 2 }).AsReadOnly();
+            var random = new Ksnm.Randoms.IncrementRandom(0);
+            for (int i = 0; i < sample.Count * 2; i++)
+            {
+                var index = sample.IndexOfRandom(random);
+                Assert.IsTrue(index >= 0);
+                Assert.IsTrue(index < sample.Count);
+            }
+        }
+    }
+}
