2185bcd56468e0406fcfbb5f6f1afdb3e0deb8c8
笠浪貴大 <dev.kacha@gmail.com>
2019-07-05 02:38:35 +0900

    Debug クラス追加 FW4.5の機能を使用するのでプロジェクト設定変更

---
+++ Ksnm/Ksnm/Debug.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Ksnm
+{
+    /// <summary>
+    /// デバッグ情報
+    /// </summary>
+    public class Debug
+    {
+        public static string GetFilePathAndLineNumber([CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = -1)
+        {
+            return $"{filePath} {lineNumber}";
+        }
+    }
+}
