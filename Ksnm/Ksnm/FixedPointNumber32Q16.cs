062fabd64dbc5e99cbcc35064f0165c0275cf536
笠浪貴大 <dev.kacha@gmail.com>
2019-08-13 09:03:25 +0900

    固定小数点数型追加

---
+++ Ksnm/Ksnm/FixedPointNumber32Q16.cs
@@ -0,0 +1,266 @@
+﻿/*
+The zlib License
+
+Copyright (c) 2019 Takahiro Kasanami
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source distribution.
+*/
+using System;
+using System.Runtime.InteropServices;
+
+namespace Ksnm
+{
+    /// <summary>
+    /// 固定小数点数(全体のビット数32、小数部分のビット数16)
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct FixedPointNumber32Q16 : IComparable, IComparable<FixedPointNumber32Q16>, IEquatable<FixedPointNumber32Q16>
+    {
+        #region 定数
+        public const int QBits = 16;
+        public readonly static FixedPointNumber32Q16 Zero = new FixedPointNumber32Q16() { Integer = 0 };
+        public readonly static FixedPointNumber32Q16 One = new FixedPointNumber32Q16() { Integer = 1 };
+        public readonly static FixedPointNumber32Q16 MinValue = new FixedPointNumber32Q16() { Bits = int.MinValue };
+        public readonly static FixedPointNumber32Q16 MaxValue = new FixedPointNumber32Q16() { Bits = int.MaxValue };
+        public readonly static FixedPointNumber32Q16 Epsilon = new FixedPointNumber32Q16() { Fractional = 1 };
+        const int OneBits = 1 << QBits;
+        #endregion 定数
+
+        #region フィールド
+        /// <summary>
+        /// 全体
+        /// </summary>
+        [FieldOffset(0)]
+        public int Bits;
+        /// <summary>
+        /// 整数部
+        /// </summary>
+        [FieldOffset(2)]
+        public short Integer;
+        /// <summary>
+        /// 小数部
+        /// </summary>
+        [FieldOffset(0)]
+        public ushort Fractional;
+        #endregion フィールド
+
+        #region 単項演算子
+        public static FixedPointNumber32Q16 operator +(FixedPointNumber32Q16 value)
+        {
+            return value;
+        }
+        public static FixedPointNumber32Q16 operator -(FixedPointNumber32Q16 value)
+        {
+            value.Bits = -value.Bits;
+            return value;
+        }
+        public static FixedPointNumber32Q16 operator ~(FixedPointNumber32Q16 value)
+        {
+            value.Bits = ~value.Bits;
+            return value;
+        }
+        #endregion 単項演算子
+
+        #region 二項演算子
+        public static FixedPointNumber32Q16 operator +(FixedPointNumber32Q16 valueL, FixedPointNumber32Q16 valueR)
+        {
+            var temp = new FixedPointNumber32Q16();
+            temp.Bits = valueL.Bits + valueR.Bits;
+            return temp;
+        }
+        public static FixedPointNumber32Q16 operator -(FixedPointNumber32Q16 valueL, FixedPointNumber32Q16 valueR)
+        {
+            var temp = new FixedPointNumber32Q16();
+            temp.Bits = valueL.Bits - valueR.Bits;
+            return temp;
+        }
+        public static FixedPointNumber32Q16 operator *(FixedPointNumber32Q16 valueL, FixedPointNumber32Q16 valueR)
+        {
+            long temp = valueL.Bits;
+            temp *= valueR.Bits;
+            temp >>= QBits;
+            return new FixedPointNumber32Q16() { Bits = (int)temp };
+        }
+        public static FixedPointNumber32Q16 operator /(FixedPointNumber32Q16 valueL, FixedPointNumber32Q16 valueR)
+        {
+            long temp = valueL.Bits;
+            temp <<= QBits;
+            temp /= valueR.Bits;
+            return new FixedPointNumber32Q16() { Bits = (int)temp };
+        }
+        public static FixedPointNumber32Q16 operator %(FixedPointNumber32Q16 valueL, FixedPointNumber32Q16 valueR)
+        {
+            return new FixedPointNumber32Q16() { Bits = valueL.Bits % valueR.Bits };
+        }
+        public static FixedPointNumber32Q16 operator &(FixedPointNumber32Q16 valueL, FixedPointNumber32Q16 valueR)
+        {
+            return new FixedPointNumber32Q16() { Bits = valueL.Bits & valueR.Bits };
+        }
+        public static FixedPointNumber32Q16 operator &(FixedPointNumber32Q16 valueL, int valueR)
+        {
+            return new FixedPointNumber32Q16() { Bits = valueL.Bits & valueR };
+        }
+        public static FixedPointNumber32Q16 operator |(FixedPointNumber32Q16 valueL, FixedPointNumber32Q16 valueR)
+        {
+            return new FixedPointNumber32Q16() { Bits = valueL.Bits | valueR.Bits };
+        }
+        public static FixedPointNumber32Q16 operator |(FixedPointNumber32Q16 valueL, int valueR)
+        {
+            return new FixedPointNumber32Q16() { Bits = valueL.Bits | valueR };
+        }
+        public static FixedPointNumber32Q16 operator ^(FixedPointNumber32Q16 valueL, FixedPointNumber32Q16 valueR)
+        {
+            return new FixedPointNumber32Q16() { Bits = valueL.Bits ^ valueR.Bits };
+        }
+        public static FixedPointNumber32Q16 operator ^(FixedPointNumber32Q16 valueL, int valueR)
+        {
+            return new FixedPointNumber32Q16() { Bits = valueL.Bits ^ valueR };
+        }
+        public static FixedPointNumber32Q16 operator <<(FixedPointNumber32Q16 value, int shift)
+        {
+            return new FixedPointNumber32Q16() { Bits = value.Bits << shift };
+        }
+        public static FixedPointNumber32Q16 operator >>(FixedPointNumber32Q16 value, int shift)
+        {
+            return new FixedPointNumber32Q16() { Bits = value.Bits >> shift };
+        }
+        #endregion 2項演算子
+
+        #region 比較演算子
+        public static bool operator ==(FixedPointNumber32Q16 valueL, FixedPointNumber32Q16 valueR)
+        {
+            return valueL.Bits == valueR.Bits;
+        }
+        public static bool operator !=(FixedPointNumber32Q16 valueL, FixedPointNumber32Q16 valueR)
+        {
+            return valueL.Bits != valueR.Bits;
+        }
+        public static bool operator >(FixedPointNumber32Q16 valueL, FixedPointNumber32Q16 valueR)
+        {
+            return valueL.Bits > valueR.Bits;
+        }
+        public static bool operator <(FixedPointNumber32Q16 valueL, FixedPointNumber32Q16 valueR)
+        {
+            return valueL.Bits < valueR.Bits;
+        }
+        public static bool operator >=(FixedPointNumber32Q16 valueL, FixedPointNumber32Q16 valueR)
+        {
+            return valueL.Bits >= valueR.Bits;
+        }
+        public static bool operator <=(FixedPointNumber32Q16 valueL, FixedPointNumber32Q16 valueR)
+        {
+            return valueL.Bits <= valueR.Bits;
+        }
+        #endregion 比較演算子
+
+        #region 型変換
+        public static implicit operator FixedPointNumber32Q16(byte value)
+        {
+            return new FixedPointNumber32Q16() { Integer = value };
+        }
+        public static implicit operator FixedPointNumber32Q16(sbyte value)
+        {
+            return new FixedPointNumber32Q16() { Integer = value };
+        }
+        public static implicit operator FixedPointNumber32Q16(short value)
+        {
+            return new FixedPointNumber32Q16() { Integer = value };
+        }
+        public static explicit operator FixedPointNumber32Q16(ushort value)
+        {
+            return new FixedPointNumber32Q16() { Integer = (short)value };
+        }
+        public static explicit operator FixedPointNumber32Q16(int value)
+        {
+            return new FixedPointNumber32Q16() { Integer = (short)value };
+        }
+        public static explicit operator FixedPointNumber32Q16(uint value)
+        {
+            return new FixedPointNumber32Q16() { Integer = (short)value };
+        }
+        public static explicit operator FixedPointNumber32Q16(long value)
+        {
+            return new FixedPointNumber32Q16() { Integer = (short)value };
+        }
+        public static explicit operator FixedPointNumber32Q16(ulong value)
+        {
+            return new FixedPointNumber32Q16() { Integer = (short)value };
+        }
+        public static explicit operator FixedPointNumber32Q16(float value)
+        {
+            return (FixedPointNumber32Q16)((double)value);
+        }
+        public static explicit operator FixedPointNumber32Q16(double value)
+        {
+            value *= OneBits;
+            return new FixedPointNumber32Q16() { Bits = (int)value };
+        }
+        public static explicit operator FixedPointNumber32Q16(decimal value)
+        {
+            value *= OneBits;
+            return new FixedPointNumber32Q16() { Bits = (int)value };
+        }
+        #endregion 型変換
+
+
+        #region IComparable
+        public int CompareTo(object obj)
+        {
+            return CompareTo((FixedPointNumber32Q16)obj);
+        }
+
+        public int CompareTo(FixedPointNumber32Q16 other)
+        {
+            return this.Bits.CompareTo(other.Bits);
+        }
+        #endregion IComparable
+
+        #region IEquatable
+        public bool Equals(FixedPointNumber32Q16 other)
+        {
+            return Bits == other.Bits;
+        }
+        #endregion IEquatable
+
+        #region object
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj is FixedPointNumber32Q16)
+            {
+                return Equals((FixedPointNumber32Q16)obj);
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return Bits.GetHashCode();
+        }
+        public override string ToString()
+        {
+            decimal temp = Bits;
+            temp /= OneBits;
+            return temp.ToString();
+        }
+        #endregion object
+    }
+}
---
+++ Ksnm/KsnmTests/FixedPointNumber32Q16Tests.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ksnm.Tests
+{
+    using Fixed = FixedPointNumber32Q16;
+
+    [TestClass()]
+    public class FixedPointNumber32Q16Tests
+    {
+        [TestMethod()]
+        public void CastTest()
+        {
+            var expected = new Fixed() { Integer = 123 };
+            Assert.AreEqual(expected, (byte)123);
+            Assert.AreEqual(expected, (sbyte)123);
+            Assert.AreEqual(expected, (short)123);
+            Assert.AreEqual(expected, (Fixed)(ushort)123);
+            Assert.AreEqual(expected, (Fixed)123);
+            Assert.AreEqual(expected, (Fixed)(uint)123);
+            Assert.AreEqual(expected, (Fixed)(long)123);
+            Assert.AreEqual(expected, (Fixed)(ulong)123);
+            Assert.AreEqual(expected, (Fixed)123.0f);
+            Assert.AreEqual(expected, (Fixed)123.0d);
+            Assert.AreEqual(expected, (Fixed)123.0m);
+        }
+        [TestMethod()]
+        public void OperationsTest1()
+        {
+            var value = (Fixed)123;
+            var expected1 = new Fixed() { Integer = +123 };
+            var expected2 = new Fixed() { Integer = -123 };
+            var expected3 = new Fixed() { Bits = ~(123 << 16) };
+            Assert.AreEqual(expected1, +value, Debug.GetFilePathAndLineNumber());
+            Assert.AreEqual(expected2, -value, Debug.GetFilePathAndLineNumber());
+            Assert.AreEqual(expected3, ~value, Debug.GetFilePathAndLineNumber());
+        }
+        [TestMethod()]
+        public void OperationsTest2()
+        {
+            var n1 = (Fixed)(-1);
+            var p0_5 = (Fixed)0.5;
+            var p1 = (Fixed)1;
+            var p1_5 = (Fixed)1.5;
+            var p2 = (Fixed)2;
+            var p2_5 = (Fixed)2.5;
+            var p3 = (Fixed)3;
+            var p4 = (Fixed)4;
+            var p6 = (Fixed)6;
+            // +
+            Assert.AreEqual(p3, p1 + p2, Debug.GetFilePathAndLineNumber());
+            Assert.AreEqual(p4, p2 + p2, Debug.GetFilePathAndLineNumber());
+            // -
+            Assert.AreEqual(p1, p3 - p2, Debug.GetFilePathAndLineNumber());
+            Assert.AreEqual(n1, p1 - p2, Debug.GetFilePathAndLineNumber());
+            // *
+            Assert.AreEqual(p2, p1 * p2, Debug.GetFilePathAndLineNumber());
+            Assert.AreEqual(p6, p2 * p3, Debug.GetFilePathAndLineNumber());
+            // /
+            Assert.AreEqual(p2, p4 / p2, Debug.GetFilePathAndLineNumber());
+            Assert.AreEqual(p1_5, p3 / p2, Debug.GetFilePathAndLineNumber());
+            // %
+            Assert.AreEqual(p1, p3 % p2, Debug.GetFilePathAndLineNumber());
+            Assert.AreEqual(p0_5, p2_5 % p2, Debug.GetFilePathAndLineNumber());
+            // &
+            Assert.AreEqual(p2, p3 & p2, Debug.GetFilePathAndLineNumber());
+            // |
+            Assert.AreEqual(p3, p1 | p2, Debug.GetFilePathAndLineNumber());
+            // ^
+            Assert.AreEqual(p2, p3 ^ p1, Debug.GetFilePathAndLineNumber());
+            // <<
+            Assert.AreEqual(p2, p1 << 1, Debug.GetFilePathAndLineNumber());
+            // >>
+            Assert.AreEqual(p1, p2 >> 1, Debug.GetFilePathAndLineNumber());
+        }
+        [TestMethod()]
+        public void OperationsTest3()
+        {
+            var value1 = (Fixed)1;
+            var value2 = (Fixed)1.1;
+            var value3 = (Fixed)1;
+
+            Assert.IsTrue(value1 != value2);
+            Assert.IsTrue(value1 == value3);
+            Assert.IsTrue(value1 < value2);
+            Assert.IsTrue(value1 <= value2);
+            Assert.IsTrue(value2 > value1);
+            Assert.IsTrue(value2 >= value1);
+        }
+        [TestMethod()]
+        public void CompareToTest()
+        {
+            var value1 = (Fixed)1;
+            var value2 = (Fixed)1.1;
+            var value3 = (Fixed)1;
+
+            Assert.AreEqual(-1, value1.CompareTo(value2));
+            Assert.AreEqual(0, value1.CompareTo(value3));
+            Assert.AreEqual(+1, value2.CompareTo(value1));
+        }
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            var value1 = (Fixed)1;
+            var value2 = (Fixed)1.1;
+            var value3 = (Fixed)1;
+
+            Assert.IsFalse(value1.Equals(value2));
+            Assert.IsTrue(value1.Equals(value3));
+        }
+    }
+}
