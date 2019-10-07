99abf726533ce08060b410ca4b6314f2e792e314
笠浪貴大 <dev.kacha@gmail.com>
2019-10-08 00:06:04 +0900

    Fraction32追加（分数）

---
+++ Ksnm/Ksnm/Numerics/Fraction32.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Ksnm.Numerics
+{
+    /// <summary>
+    /// 分数
+    /// </summary>
+    public struct Fraction32 : IComparable, IComparable<Fraction32>, IEquatable<Fraction32>
+    {
+        /// <summary>
+        /// 分子
+        /// </summary>
+        public int Numerator { get; private set; }
+        /// <summary>
+        /// 分母
+        /// </summary>
+        public int Denominator { get; private set; }
+        /// <summary>
+        /// 分子と分母を指定して初期化
+        /// </summary>
+        /// <param name="numerator">分子</param>
+        /// <param name="denominator">分母</param>
+        public Fraction32(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+        /// <summary>
+        /// 分子を指定して初期化
+        /// </summary>
+        /// <param name="numerator">分子</param>
+        public Fraction32(int numerator)
+        {
+            Numerator = numerator;
+            Denominator = 1;
+        }
+
+        #region IComparable
+        /// <summary>
+        /// 比較し、これらの相対値を示す値を返します。
+        /// </summary>
+        /// <param name="obj">比較するオブジェクト</param>
+        /// <returns>0の場合等価です。0 より大きい値の場合 obj よりも大きいです。0 より小さい値の場合 obj よりも小さいです。</returns>
+        public int CompareTo(object obj)
+        {
+            return CompareTo((Fraction32)obj);
+        }
+        /// <summary>
+        /// 比較し、これらの相対値を示す値を返します。
+        /// </summary>
+        /// <param name="other">比較対象の固定小数点数</param>
+        /// <returns>0の場合等価です。0 より大きい値の場合 obj よりも大きいです。0 より小さい値の場合 obj よりも小さいです。</returns>
+        public int CompareTo(Fraction32 other)
+        {
+            other.Numerator *= Denominator;
+            return (Numerator * other.Denominator).CompareTo(other.Numerator);
+        }
+        #endregion IComparable
+
+        #region IEquatable
+        /// <summary>
+        /// 指定したオブジェクトが、現在のオブジェクトと等しいかどうかを判断します。
+        /// </summary>
+        /// <returns>指定したオブジェクトが現在のオブジェクトと等しい場合は true。それ以外の場合は false。</returns>
+        public bool Equals(Fraction32 other)
+        {
+            return CompareTo(other) == 0;
+        }
+        #endregion IEquatable
+
+        #region object
+        /// <summary>
+        /// 指定したオブジェクトが、現在のオブジェクトと等しいかどうかを判断します。
+        /// </summary>
+        /// <returns>指定したオブジェクトが現在のオブジェクトと等しい場合は true。それ以外の場合は false。</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj is Fraction32)
+            {
+                return Equals((Fraction32)obj);
+            }
+            return false;
+        }
+        /// <summary>
+        /// このインスタンスのハッシュ コードを返します。
+        /// </summary>
+        public override int GetHashCode()
+        {
+            return Numerator.GetHashCode() ^ Denominator.GetHashCode();
+        }
+        /// <summary>
+        /// このインスタンスの数値を、それと等価な文字列形式に変換します。
+        /// </summary>
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+        #endregion object
+    }
+}
---
+++ Ksnm/KsnmTests/Numerics/Fraction32Tests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ksnm.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksnm.Numerics.Tests
+{
+    [TestClass()]
+    public class Fraction32Tests
+    {
+        [TestMethod()]
+        public void CompareToTest()
+        {
+            var a = new Fraction32(1, 2);// 0.5
+            var b = new Fraction32(2, 3);// 0.666...
+            var c = new Fraction32(4, 5);// 0.8
+            var d = new Fraction32(2, 4);// 0.5
+
+            Assert.AreEqual(-1, a.CompareTo(b));
+            Assert.AreEqual(-1, a.CompareTo(c));
+            Assert.AreEqual(-1, b.CompareTo(c));
+
+            Assert.AreEqual(0, a.CompareTo(a));
+            Assert.AreEqual(0, b.CompareTo(b));
+            Assert.AreEqual(0, c.CompareTo(c));
+            Assert.AreEqual(0, a.CompareTo(d));
+
+            Assert.AreEqual(+1, b.CompareTo(a));
+            Assert.AreEqual(+1, c.CompareTo(a));
+            Assert.AreEqual(+1, c.CompareTo(b));
+        }
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            var a = new Fraction32(1, 2);// 0.5
+            var b = new Fraction32(2, 3);// 0.666...
+            var c = new Fraction32(4, 5);// 0.8
+            var d = new Fraction32(2, 4);// 0.5
+
+            Assert.IsTrue(a.Equals(a));
+            Assert.IsTrue(a.Equals(d));
+
+            Assert.IsFalse(a.Equals(b));
+            Assert.IsFalse(a.Equals(c));
+            Assert.IsFalse(b.Equals(a));
+            Assert.IsFalse(b.Equals(c));
+            Assert.IsFalse(c.Equals(a));
+            Assert.IsFalse(c.Equals(b));
+        }
+
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+            var a = new Fraction32(1, 2);
+            var b = new Fraction32(2, 3);
+
+            Assert.IsTrue(a.GetHashCode() == a.GetHashCode());
+            Assert.IsFalse(a.GetHashCode() == b.GetHashCode());
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            var a = new Fraction32(1, 2);
+            var b = new Fraction32(2, 3);
+
+            Assert.AreEqual("1/2", a.ToString());
+            Assert.AreEqual("2/3", b.ToString());
+        }
+    }
+}
