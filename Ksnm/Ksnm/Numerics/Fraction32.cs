ba31886ce72b969881c920a19e75f30126af82b1
笠浪貴大 <dev.kacha@gmail.com>
2019-10-08 15:14:08 +0900

    型変換追加

--- Ksnm/Ksnm/Numerics/Fraction32.cs
+++ Ksnm/Ksnm/Numerics/Fraction32.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Ksnm.ExtensionMethods.System.Decimal;
+using Ksnm.ExtensionMethods.System.Double;
+using System;
 
 namespace Ksnm.Numerics
 {
@@ -34,6 +36,206 @@ namespace Ksnm.Numerics
             Numerator = numerator;
             Denominator = 1;
         }
+        /// <summary>
+        /// 実数を分数に変換して初期化
+        /// </summary>
+        /// <param name="value">実数</param>
+        public Fraction32(double value) : this((decimal)value)
+        {
+#if false
+            if (value >= 0)
+            {
+                Numerator = (int)value;
+                Denominator = 1;
+            }
+            else
+            {
+                Numerator = (int)(value.GetMantissa() >> (52 - 31));
+                Denominator = 1 << -value.GetExponent();
+            }
+#endif
+        }
+        /// <summary>
+        /// 実数を分数に変換して初期化
+        /// </summary>
+        /// <param name="value">実数</param>
+        public Fraction32(decimal value)
+        {
+            var mantissa = value.GetMantissa();
+            var exponent = value.GetExponentBits();
+            var denominator = System.Math.Pow(10, exponent);
+            Numerator = (int)mantissa;
+            Denominator = (int)denominator;
+            if (value < 0)
+            {
+                Numerator = -Numerator;
+            }
+        }
+
+        #region 型変換
+        #region 他の型→分数型
+        /// <summary>
+        /// byte から 分数型 への暗黙的な変換を定義します。
+        /// </summary>
+        public static implicit operator Fraction32(byte value)
+        {
+            return new Fraction32(value);
+        }
+        /// <summary>
+        /// sbyte から 分数型 への暗黙的な変換を定義します。
+        /// </summary>
+        public static implicit operator Fraction32(sbyte value)
+        {
+            return new Fraction32(value);
+        }
+        /// <summary>
+        /// short から 分数型 への暗黙的な変換を定義します。
+        /// </summary>
+        public static implicit operator Fraction32(short value)
+        {
+            return new Fraction32(value);
+        }
+        /// <summary>
+        /// ushort から 分数型 への暗黙的な変換を定義します。
+        /// </summary>
+        public static implicit operator Fraction32(ushort value)
+        {
+            return new Fraction32(value);
+        }
+        /// <summary>
+        /// int から 分数型 への暗黙的な変換を定義します。
+        /// </summary>
+        public static implicit operator Fraction32(int value)
+        {
+            return new Fraction32(value);
+        }
+        /// <summary>
+        /// uint から 分数型 への明示的な変換を定義します。
+        /// </summary>
+        public static explicit operator Fraction32(uint value)
+        {
+            return new Fraction32((int)value);
+        }
+        /// <summary>
+        /// long から 分数型 への明示的な変換を定義します。
+        /// </summary>
+        public static explicit operator Fraction32(long value)
+        {
+            return new Fraction32((int)value);
+        }
+        /// <summary>
+        /// ulong から 分数型 への明示的な変換を定義します。
+        /// </summary>
+        public static explicit operator Fraction32(ulong value)
+        {
+            return new Fraction32((int)value);
+        }
+        /// <summary>
+        /// float から 分数型 への明示的な変換を定義します。
+        /// </summary>
+        public static explicit operator Fraction32(float value)
+        {
+            return (Fraction32)(double)value;
+        }
+        /// <summary>
+        /// double から 分数型 への明示的な変換を定義します。
+        /// </summary>
+        public static explicit operator Fraction32(double value)
+        {
+            return new Fraction32(value);
+        }
+        /// <summary>
+        /// decimal から 分数型 への明示的な変換を定義します。
+        /// </summary>
+        public static explicit operator Fraction32(decimal value)
+        {
+            return new Fraction32(value);
+        }
+        #endregion 他の型→分数型
+        #region 分数型→他の型
+        /// <summary>
+        /// 分数型 から byte への明示的な変換を定義します。
+        /// </summary>
+        public static explicit operator byte(Fraction32 value)
+        {
+            return (byte)(int)value;
+        }
+        /// <summary>
+        /// 分数型 から sbyte への明示的な変換を定義します。
+        /// </summary>
+        public static explicit operator sbyte(Fraction32 value)
+        {
+            return (sbyte)(int)value;
+        }
+        /// <summary>
+        /// 分数型 から short への明示的な変換を定義します。
+        /// </summary>
+        public static explicit operator short(Fraction32 value)
+        {
+            return (short)(int)value;
+        }
+        /// <summary>
+        /// 分数型 から ushort への明示的な変換を定義します。
+        /// </summary>
+        public static explicit operator ushort(Fraction32 value)
+        {
+            return (ushort)(int)value;
+        }
+        /// <summary>
+        /// 分数型 から int への明示的な変換を定義します。
+        /// </summary>
+        public static explicit operator int(Fraction32 value)
+        {
+            return value.Numerator / value.Denominator;
+        }
+        /// <summary>
+        /// 分数型 から uint への明示的な変換を定義します。
+        /// </summary>
+        public static explicit operator uint(Fraction32 value)
+        {
+            return (uint)(int)value;
+        }
+        /// <summary>
+        /// 分数型 から long への明示的な変換を定義します。
+        /// </summary>
+        public static explicit operator long(Fraction32 value)
+        {
+            return (int)value;
+        }
+        /// <summary>
+        /// 分数型 から ulong への明示的な変換を定義します。
+        /// </summary>
+        public static explicit operator ulong(Fraction32 value)
+        {
+            return (ulong)(int)value;
+        }
+        /// <summary>
+        /// 分数型 から float への明示的な変換を定義します。
+        /// </summary>
+        public static explicit operator float(Fraction32 value)
+        {
+            return (float)(double)value;
+        }
+        /// <summary>
+        /// 分数型 から double への暗黙的な変換を定義します。
+        /// </summary>
+        public static implicit operator double(Fraction32 value)
+        {
+            double temp = value.Numerator;
+            temp /= value.Denominator;
+            return temp;
+        }
+        /// <summary>
+        /// 分数型 から double への暗黙的な変換を定義します。
+        /// </summary>
+        public static implicit operator decimal(Fraction32 value)
+        {
+            decimal temp = value.Numerator;
+            temp /= value.Denominator;
+            return temp;
+        }
+        #endregion 分数型→他の型
+        #endregion 型変換
 
         #region IComparable
         /// <summary>
--- Ksnm/KsnmTests/Numerics/Fraction32Tests.cs
+++ Ksnm/KsnmTests/Numerics/Fraction32Tests.cs
@@ -11,6 +11,126 @@ namespace Ksnm.Numerics.Tests
     [TestClass()]
     public class Fraction32Tests
     {
+        const float LoopIncrement = 1.0f / 8;
+        [TestMethod()]
+        public void CastTest()
+        {
+            // byte
+            for (byte i = 0; i <= 10; i++)
+            {
+                var fr = (Fraction32)i;
+                Assert.AreEqual(i, (byte)fr, $"byte i={i} fr={fr}");
+            }
+            for (double d = 0; d <= 10; d += LoopIncrement)
+            {
+                var i = (byte)d;
+                var fr = (Fraction32)d;
+                Assert.AreEqual(i, (byte)fr, $"byte i={i} fr={fr}");
+            }
+            // sbyte
+            for (sbyte i = -10; i <= 10; i++)
+            {
+                var fr = (Fraction32)i;
+                Assert.AreEqual(i, (sbyte)fr, $"sbyte i={i} fr={fr}");
+            }
+            for (double d = -10; d <= 10; d += LoopIncrement)
+            {
+                var i = (sbyte)d;
+                var fr = (Fraction32)d;
+                Assert.AreEqual(i, (sbyte)fr, $"sbyte i={i} fr={fr}");
+            }
+            // short
+            for (short i = -10; i <= 10; i++)
+            {
+                var fr = (Fraction32)i;
+                Assert.AreEqual(i, (short)fr, $"short i={i} fr={fr}");
+            }
+            for (double d = -10; d <= 10; d += LoopIncrement)
+            {
+                var i = (short)d;
+                var fr = (Fraction32)d;
+                Assert.AreEqual(i, (short)fr, $"short i={i} fr={fr}");
+            }
+            // ushort
+            for (ushort i = 0; i <= 10; i++)
+            {
+                var fr = (Fraction32)i;
+                Assert.AreEqual(i, (ushort)fr, $"ushort i={i} fr={fr}");
+            }
+            for (double d = 0; d <= 10; d += LoopIncrement)
+            {
+                var i = (ushort)d;
+                var fr = (Fraction32)d;
+                Assert.AreEqual(i, (ushort)fr, $"ushort i={i} fr={fr}");
+            }
+            // int
+            for (int i = -10; i <= 10; i++)
+            {
+                var fr = (Fraction32)i;
+                Assert.AreEqual(i, (int)fr, $"int i={i} fr={fr}");
+            }
+            for (double d = -10; d <= 10; d += LoopIncrement)
+            {
+                var i = (int)d;
+                var fr = (Fraction32)d;
+                Assert.AreEqual(i, (int)fr, $"int i={i} fr={fr}");
+            }
+            // uint
+            for (uint i = 0; i <= 10; i++)
+            {
+                var fr = (Fraction32)i;
+                Assert.AreEqual(i, (uint)fr, $"uint i={i} fr={fr}");
+            }
+            for (double d = 0; d <= 10; d += LoopIncrement)
+            {
+                var i = (uint)d;
+                var fr = (Fraction32)d;
+                Assert.AreEqual(i, (uint)fr, $"uint i={i} fr={fr}");
+            }
+            // long
+            for (long i = -10; i <= 10; i++)
+            {
+                var fr = (Fraction32)i;
+                Assert.AreEqual(i, (long)fr, $"long i={i} fr={fr}");
+            }
+            for (double d = -10; d <= 10; d += LoopIncrement)
+            {
+                var i = (long)d;
+                var fr = (Fraction32)d;
+                Assert.AreEqual(i, (long)fr, $"long i={i} fr={fr}");
+            }
+            // ulong
+            for (ulong i = 0; i <= 10; i++)
+            {
+                var fr = (Fraction32)i;
+                Assert.AreEqual(i, (ulong)fr, $"ulong i={i} fr={fr}");
+            }
+            for (double d = 0; d <= 10; d += LoopIncrement)
+            {
+                var i = (ulong)d;
+                var fr = (Fraction32)d;
+                Assert.AreEqual(i, (ulong)fr, $"ulong i={i} fr={fr}");
+            }
+            // float
+            for (float i = -10; i <= 10; i += LoopIncrement)
+            {
+                var fr = (Fraction32)i;
+                Assert.AreEqual(i, (float)fr, $"float i={i} fr={fr}");
+            }
+            // double
+            for (double i = -10; i <= 10; i += LoopIncrement)
+            {
+                var fr = (Fraction32)i;
+                Assert.AreEqual(i, (double)fr, $"double i={i} fr={fr}");
+            }
+            // decimal
+            for (decimal i = -10; i <= 10; i += 0.125M)
+            {
+                var fr = (Fraction32)i;
+                Assert.AreEqual(i, (decimal)fr, $"decimal i={i} fr={fr}");
+            }
+        }
+
         [TestMethod()]
         public void CompareToTest()
         {
