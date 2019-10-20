446f0188c186f10123e6e2a615d81d14640eabf3
笠浪貴大 <dev.kacha@gmail.com>
2019-10-20 21:25:37 +0900

    System.Text.Encoding に無い機能を定義した Encoding 追加

---
+++ Ksnm/KsnmTests/Text/EncodingTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ksnm.Text.Tests
+{
+    [TestClass()]
+    public class EncodingTests
+    {
+        [TestMethod()]
+        public void PropertiesTest()
+        {
+            var expectedEncoding = System.Text.Encoding.GetEncoding("Shift_JIS");
+            var actualEncoding = Encoding.Shift_JIS;
+            Assert.AreEqual(expectedEncoding, actualEncoding);
+        }
+    }
+}
