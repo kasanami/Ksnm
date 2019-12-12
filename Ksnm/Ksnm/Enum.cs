bf304d96282652cc52ef8bfc8aa90a68a98670ad
笠浪貴大 <dev.kacha@gmail.com>
2019-12-13 06:45:48 +0900

    Parse 関数追加

--- Ksnm/Ksnm/Enum.cs
+++ Ksnm/Ksnm/Enum.cs
@@ -37,5 +37,32 @@ namespace Ksnm
         {
             return (T[])System.Enum.GetValues(typeof(T));
         }
+        /// <summary>
+        /// 文字列形式での 1 つ以上の列挙定数の名前または数値を、等価の列挙オブジェクトに変換します。
+        /// </summary>
+        /// <typeparam name="T">列挙型。</typeparam>
+        /// <param name="value">変換する名前または値が含まれている文字列。</param>
+        /// <returns>value によって表される値を持つ T 型のオブジェクト。</returns>
+        /// <exception cref="System.ArgumentNullException">value が null です。</exception>
+        /// <exception cref="System.ArgumentException">value が空の文字列であるか、または空白しか含まれていません。 または value は名前ですが、列挙に対して定義されている名前付き定数の名前ではありません。</exception>
+        /// <exception cref="System.OverflowException">value は T の基になる型の範囲外です。</exception>
+        public static T Parse<T>(string value) where T : System.Enum
+        {
+            return (T)System.Enum.Parse(typeof(T), value);
+        }
+        /// <summary>
+        /// 文字列形式での 1 つ以上の列挙定数の名前または数値を、等価の列挙オブジェクトに変換します。
+        /// </summary>
+        /// <typeparam name="T">列挙型。</typeparam>
+        /// <param name="value">変換する名前または値が含まれている文字列。</param>
+        /// <param name="ignoreCase">大文字と小文字を区別しない場合は true。大文字と小文字を区別する場合は false。</param>
+        /// <returns>T型のオブジェクト。</returns>
+        /// <exception cref="System.ArgumentNullException">value が null です。</exception>
+        /// <exception cref="System.ArgumentException">value が空の文字列 ("") であるか、または空白しか含まれていません。 または value は名前ですが、列挙に対して定義されている名前付き定数の名前ではありません。</exception>
+        /// <exception cref="System.OverflowException">value は T の基になる型の範囲外です。</exception>
+        public static T Parse<T>(string value, bool ignoreCase) where T:System.Enum
+        {
+            return (T)System.Enum.Parse(typeof(T), value, ignoreCase);
+        }
     }
 }
--- Ksnm/KsnmTests/EnumTests.cs
+++ Ksnm/KsnmTests/EnumTests.cs
@@ -20,5 +20,46 @@ namespace Ksnm.Tests
             var hogeValues = Enum.GetValues<Hoge>();
             Assert.AreEqual("A,B,C,D", hogeValues.ToJoinedString(","));
         }
+
+        [TestMethod()]
+        public void ParseTest()
+        {
+            var A = Enum.Parse<Hoge>("A");
+            Assert.AreEqual(Hoge.A, A);
+            var B = Enum.Parse<Hoge>("B");
+            Assert.AreEqual(Hoge.B, B);
+            var C = Enum.Parse<Hoge>("C", true);
+            Assert.AreEqual(Hoge.C, C);
+
+            // 例外テスト
+            try
+            {
+                var c = Enum.Parse<Hoge>("c");
+                Assert.AreEqual(Hoge.C, c);
+            }
+            catch (System.ArgumentException) { }
+            try
+            {
+                var c = Enum.Parse<Hoge>("c", false);
+                Assert.AreEqual(Hoge.C, c);
+            }
+            catch (System.ArgumentException) { }
+
+            try
+            {
+                Enum.Parse<Hoge>(null);
+            }
+            catch (System.ArgumentNullException) { }
+            try
+            {
+                Enum.Parse<Hoge>("");
+            }
+            catch (System.ArgumentException) { }
+            try
+            {
+                Enum.Parse<Hoge>("Z");
+            }
+            catch (System.ArgumentException) { }
+        }
     }
 }
