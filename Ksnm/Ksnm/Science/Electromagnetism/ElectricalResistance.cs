2e3cc35e1c3064a69e0820e7fbdf28a5e716060b
笠浪貴大 <dev.kacha@gmail.com>
2021-04-03 23:04:41 +0900

    Squashed commit of the following:
    commit e90f4f3fb54466a7282392654e9c458738a9f45b
    Author: 笠浪貴大 <dev.kacha@gmail.com>
    Date:   Sat Apr 3 14:14:49 2021 +0900
    
        単体テスト追加
    
    commit 806be73262fa0c1d817531d0a6c6537498519efc
    Author: 笠浪貴大 <dev.kacha@gmail.com>
    Date:   Sat Apr 3 14:09:16 2021 +0900
    
        ElectricalResistance　追加

---
+++ Ksnm/Ksnm/Science/Electromagnetism/ElectricalResistance.cs
@@ -0,0 +1,57 @@
+﻿/*
+The zlib License
+
+Copyright (c) 2021 Takahiro Kasanami
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source distribution.
+*/
+using System.Linq;
+
+namespace Ksnm.Science.Electromagnetism
+{
+    /// <summary>
+    /// 電気抵抗
+    /// </summary>
+    public class ElectricalResistance
+    {
+        /// <summary>
+        /// 直列接続の合成抵抗を計算する。
+        /// </summary>
+        public static double SeriesConnection(params double[] resistances)
+        {
+            return resistances.Sum();
+        }
+        /// <summary>
+        /// 並列接続の合成抵抗を計算する。
+        /// </summary>
+        public static double ParallelConnection(params double[] resistances)
+        {
+            if (resistances.Length == 1)
+            {
+                return resistances[0];
+            }
+            else if (resistances.Length == 2)
+            {
+                // 2個の場合は和分の積
+                return (resistances[0] * resistances[1]) / (resistances[0] + resistances[1]);
+            }
+            return 1 / resistances.Select(r => 1 / r).Sum();
+        }
+    }
+}
