68da10cb81d1f69247d095152a1a8a99e02c20c5
笠浪貴大 <dev.kacha@gmail.com>
2021-04-18 00:28:03 +0900

    ElectricCurrent Ampere 追加

---
+++ Ksnm/Ksnm/Units/SI/Ampere.cs
@@ -0,0 +1,59 @@
+﻿/*
+The zlib License
+
+Copyright (c) 2021 Takahiro Kasanami
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source distribution.
+*/
+using Ksnm.Numerics;
+
+namespace Ksnm.Units.SI
+{
+    /// <summary>
+    /// アンペア
+    /// </summary>
+    public class Ampere<T> : ElectricCurrent<T> where T : IMath<T>
+    {
+        #region プロパティ
+        /// <summary>
+        /// 名前
+        /// </summary>
+        public override string Name => "ampere";
+        /// <summary>
+        /// 記号
+        /// </summary>
+        public override string Symbol => "A";
+        #endregion プロパティ
+        #region コンストラクタ
+        /// <summary>
+        /// 0 で初期化
+        /// </summary>
+        public Ampere()
+        {
+        }
+        /// <summary>
+        /// 指定した値で初期化
+        /// </summary>
+        public Ampere(T value)
+        {
+            Value = value;
+        }
+        #endregion コンストラクタ
+    }
+}
