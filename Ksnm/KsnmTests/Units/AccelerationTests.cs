f67b759ed58b90b70ef83a171f13a16806c740bf
笠浪貴大 <dev.kacha@gmail.com>
2021-04-24 22:41:25 +0900

    ファイルのコミット忘れ

---
+++ Ksnm/KsnmTests/Units/AccelerationTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ksnm.Units.GS;
+using Ksnm.Units.SI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ksnm.Numerics;
+using static Ksnm.Units.SIUnits<Ksnm.Numerics.Decimal>;
+using static Ksnm.Units.GSUnits<Ksnm.Numerics.Decimal>;
+
+namespace Ksnm.Units.Tests
+{
+    [TestClass()]
+    public class AccelerationTests
+    {
+        [TestMethod()]
+        public void AccelerationTest()
+        {
+            var g = 1 * StandardGravity;
+            var g_ms = (MetrePerSecondSquared<Decimal>)g;
+            Assert.AreEqual("9.80665m/s^2", g_ms.ToString());
+
+            var g2 = (StandardGravity<Decimal>)g_ms;
+            Assert.AreEqual("1G", g2.ToString());
+        }
+    }
+}
