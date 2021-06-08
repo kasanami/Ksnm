27ca3a2c2f23adbbfcb55c5d46e6cea88d119c9a
笠浪貴大 <dev.kacha@gmail.com>
2021-06-08 15:38:46 +0900

    Wilsons 追加

---
+++ Ksnm/Ksnm/Science/Mathematics/Theorem.cs
@@ -0,0 +1,19 @@
+﻿namespace Ksnm.Science.Mathematics
+{
+    /// <summary>
+    /// 数学の定理
+    /// </summary>
+    public class Theorem
+    {
+        /// <summary>
+        /// ウィルソンの定理
+        /// p が大きくなるにつれて計算量が膨大になるため、素数を判定するために用いるには実用的ではない。
+        /// ※ 1 は例外で true となる。
+        /// </summary>
+        /// <returns>p が素数ならば true</returns>
+        public static bool Wilsons(int p)
+        {
+            return (Math.Factorial(p - 1) + 1) % p == 0;
+        }
+    }
+}
