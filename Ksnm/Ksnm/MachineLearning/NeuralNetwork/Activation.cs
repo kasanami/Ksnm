b089170daa9c19829d951d4d47a62556c5d8c346
笠浪貴大 <dev.kacha@gmail.com>
2021-07-28 22:22:57 +0900

    Name 追加

--- Ksnm/Ksnm/MachineLearning/NeuralNetwork/Activation.cs
+++ Ksnm/Ksnm/MachineLearning/NeuralNetwork/Activation.cs
@@ -35,6 +35,10 @@ namespace Ksnm.MachineLearning.NeuralNetwork
     public class Activation
     {
         /// <summary>
+        /// 名前
+        /// </summary>
+        public string Name { get; private set; }
+        /// <summary>
         /// 活性化関数
         /// </summary>
         public Activations.Delegate Function { get; private set; }
@@ -43,12 +47,14 @@ namespace Ksnm.MachineLearning.NeuralNetwork
         /// </summary>
         public Activations.Delegate DerivativeFunction { get; private set; }
         /// <summary>
-        /// 
+        /// 指定した値で初期化する。
         /// </summary>
-        /// <param name="function"></param>
-        /// <param name="derivativeFunction"></param>
-        public Activation(Activations.Delegate function, Activations.Delegate derivativeFunction)
+        /// <param name="name">名前</param>
+        /// <param name="function">活性化関数</param>
+        /// <param name="derivativeFunction">導関数</param>
+        public Activation(string name, Activations.Delegate function, Activations.Delegate derivativeFunction)
         {
+            Name = name;
             Function = function;
             DerivativeFunction = derivativeFunction;
         }
@@ -56,27 +62,36 @@ namespace Ksnm.MachineLearning.NeuralNetwork
         /// <summary>
         /// 恒等関数
         /// </summary>
-        public static readonly Activation Identity = new Activation(Activations.Identity, Activations.DerIdentity);
+        public static readonly Activation Identity = new Activation("Identity", Activations.Identity, Activations.DerIdentity);
         /// <summary>
         /// LeakyReLU
         /// </summary>
-        public static readonly Activation LeakyReLU = new Activation(Activations.LeakyReLU, Activations.DerLeakyReLU);
+        public static readonly Activation LeakyReLU = new Activation("LeakyReLU", Activations.LeakyReLU, Activations.DerLeakyReLU);
         /// <summary>
         /// シグモイド関数
         /// </summary>
-        public static readonly Activation Sigmoid = new Activation(Activations.Sigmoid, Activations.DerSigmoid);
+        public static readonly Activation Sigmoid = new Activation("Sigmoid", Activations.Sigmoid, Activations.DerSigmoid);
         /// <summary>
         /// ソフトプラス関数
         /// </summary>
-        public static readonly Activation Softplus = new Activation(Activations.Softplus, Activations.DerSoftplus);
+        public static readonly Activation Softplus = new Activation("Softplus", Activations.Softplus, Activations.DerSoftplus);
         /// <summary>
         /// 双曲線正接関数
         /// </summary>
-        public static readonly Activation Tanh = new Activation(Activations.Tanh, Activations.DerTanh);
+        public static readonly Activation Tanh = new Activation("Tanh", Activations.Tanh, Activations.DerTanh);
         /// <summary>
         /// 正規化線形関数
         /// </summary>
-        public static readonly Activation ReLU = new Activation(Activations.ReLU, Activations.DerReLU);
+        public static readonly Activation ReLU = new Activation("ReLU", Activations.ReLU, Activations.DerReLU);
+        #endregion
+        #region
+        /// <summary>
+        /// 
+        /// </summary>
+        public override string ToString()
+        {
+            return Name;
+        }
         #endregion
     }
 }
