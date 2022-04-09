f82559bfd40e68edb4226888d840d6ce7da8c377
笠浪貴大 <dev.kacha@gmail.com>
2022-04-09 22:09:15 +0900

    不要なusing削除

--- Ksnm/Ksnm/Numerics/Imaginary.cs
+++ Ksnm/Ksnm/Numerics/Imaginary.cs
@@ -22,10 +22,6 @@ freely, subject to the following restrictions:
 3. This notice may not be removed or altered from any source distribution.
 */
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ksnm.Numerics
 {
