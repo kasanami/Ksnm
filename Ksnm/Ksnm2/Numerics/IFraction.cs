4ad8464742bc64368facb4e8a8806fd9574b0aca
笠浪貴大 <dev.kacha@gmail.com>
2023-04-11 01:05:33 +0900

    Ksnm2追加

---
+++ Ksnm/Ksnm2/Numerics/IFraction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksnm2.Numerics
+{
+    /// <summary>
+    /// 分数型インターフェイス
+    /// </summary>
+    interface IFraction<T> where T : INumber<T>
+    {
+    }
+}
