3c4bbf4d7f53bcf90c6f323bdb9dab9d9aabf4ce
笠浪貴大 <dev.kacha@gmail.com>
2024-07-31 18:13:52 +0900

    型名と区別がつかなくなるので、定数だけどローワーキャメルケースに変更

--- Ksnm/Ksnm/Units/Constants.cs
+++ Ksnm/Ksnm/Units/Constants.cs
@@ -25,6 +25,7 @@ using Ksnm.Numerics;
 using Ksnm.Units.GS;
 using Ksnm.Units.NonSI;
 using Ksnm.Units.SI;
+using System.Numerics;
 
 namespace Ksnm.Units
 {
@@ -32,45 +33,45 @@ namespace Ksnm.Units
     /// 各単位の定数を定義
     /// <para>使用例 : var mass = 123 * Gram;</para>
     /// </summary>
-    public static class Constants<T> where T : IMath<T>
+    public static class Constants<T> where T : INumber<T>
     {
         #region GS
-        public static readonly KilogramForce<T> KilogramForce = new KilogramForce<T>(1);
-        public static readonly StandardGravity<T> StandardGravity = new StandardGravity<T>(1);
+        public static readonly KilogramForce<T> kilogramForce = new KilogramForce<T>(1);
+        public static readonly StandardGravity<T> standardGravity = new StandardGravity<T>(1);
         #endregion GS
         #region NonSI
-        public static readonly Calorie<T> Calorie = new Calorie<T>(1);
-        public static readonly Knot<T> Knot = new Knot<T>(1);
-        public static readonly NauticalMile<T> NauticalMile = new NauticalMile<T>(1);
+        public static readonly Calorie<T> calorie = new Calorie<T>(1);
+        public static readonly Knot<T> knot = new Knot<T>(1);
+        public static readonly NauticalMile<T> nauticalMile = new NauticalMile<T>(1);
         #endregion NonSI
         #region SI
-        public static readonly Ampere<T> Ampere = new Ampere<T>(1);
-        public static readonly Candela<T> Candela = new Candela<T>(1);
-        public static readonly Coulomb<T> Coulomb = new Coulomb<T>(1);
-        public static readonly CubicMetre<T> CubicMetre = new CubicMetre<T>(1);
-        public static readonly DegreeCelsius<T> DegreeCelsius = new DegreeCelsius<T>(1);
-        public static readonly Gram<T> Gram = new Gram<T>(1);
-        public static readonly Hour<T> Hour = new Hour<T>(1);
-        public static readonly Joule<T> Joule = new Joule<T>(1);
-        public static readonly Kelvin<T> Kelvin = new Kelvin<T>(1);
-        public static readonly Kilogram<T> Kilogram = new Kilogram<T>(1);
-        public static readonly KiloMetrePerHour<T> KiloMetrePerHour = new KiloMetrePerHour<T>(1);
-        public static readonly Litre<T> Litre = new Litre<T>(1);
-        public static readonly Lumen<T> Lumen = new Lumen<T>(1);
-        public static readonly Lux<T> Lux = new Lux<T>(1);
-        public static readonly Metre<T> Metre = new Metre<T>(1);
-        public static readonly MetrePerSecond<T> MetrePerSecond = new MetrePerSecond<T>(1);
-        public static readonly MetrePerSecondSquared<T> MetrePerSecondSquared = new MetrePerSecondSquared<T>(1);
-        public static readonly Minute<T> Minute = new Minute<T>(1);
-        public static readonly Mole<T> Mole = new Mole<T>(1);
-        public static readonly Newton<T> Newton = new Newton<T>(1);
-        public static readonly Pascal<T> Pascal = new Pascal<T>(1);
-        public static readonly Radian<T> Radian = new Radian<T>(1);
-        public static readonly Second<T> Second = new Second<T>(1);
-        public static readonly SquareMetre<T> SquareMetre = new SquareMetre<T>(1);
-        public static readonly Steradian<T> Steradian = new Steradian<T>(1);
-        public static readonly Volt<T> Volt = new Volt<T>(1);
-        public static readonly Watt<T> Watt = new Watt<T>(1);
+        public static readonly Ampere<T> ampere = new Ampere<T>(1);
+        public static readonly Candela<T> candela = new Candela<T>(1);
+        public static readonly Coulomb<T> coulomb = new Coulomb<T>(1);
+        public static readonly CubicMetre<T> cubicMetre = new CubicMetre<T>(1);
+        public static readonly DegreeCelsius<T> degreeCelsius = new DegreeCelsius<T>(1);
+        public static readonly Gram<T> gram = new Gram<T>(1);
+        public static readonly Hour<T> hour = new Hour<T>(1);
+        public static readonly Joule<T> joule = new Joule<T>(1);
+        public static readonly Kelvin<T> kelvin = new Kelvin<T>(1);
+        public static readonly Kilogram<T> kilogram = new Kilogram<T>(1);
+        public static readonly KiloMetrePerHour<T> kiloMetrePerHour = new KiloMetrePerHour<T>(1);
+        public static readonly Litre<T> litre = new Litre<T>(1);
+        public static readonly Lumen<T> lumen = new Lumen<T>(1);
+        public static readonly Lux<T> lux = new Lux<T>(1);
+        public static readonly Metre<T> metre = new Metre<T>(1);
+        public static readonly MetrePerSecond<T> metrePerSecond = new MetrePerSecond<T>(1);
+        public static readonly MetrePerSecondSquared<T> metrePerSecondSquared = new MetrePerSecondSquared<T>(1);
+        public static readonly Minute<T> minute = new Minute<T>(1);
+        public static readonly Mole<T> mole = new Mole<T>(1);
+        public static readonly Newton<T> newton = new Newton<T>(1);
+        public static readonly Pascal<T> pascal = new Pascal<T>(1);
+        public static readonly Radian<T> radian = new Radian<T>(1);
+        public static readonly Second<T> second = new Second<T>(1);
+        public static readonly SquareMetre<T> squareMetre = new SquareMetre<T>(1);
+        public static readonly Steradian<T> steradian = new Steradian<T>(1);
+        public static readonly Volt<T> volt = new Volt<T>(1);
+        public static readonly Watt<T> watt = new Watt<T>(1);
         #endregion SI
     }
 }
