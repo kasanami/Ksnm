07817e53bbc38e36573bac3f23e1f4492c58bf59
笠浪貴大 <dev.kacha@gmail.com>
2024-08-19 01:21:22 +0900

    ExtendedDouble追加

---
+++ Ksnm/Ksnm/Numerics/ExtendedDouble.cs
@@ -0,0 +1,79 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Ksnm.Numerics
+{
+    using UInt = System.UInt64;
+    /// <summary>
+    /// 64bit浮動小数点数型
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct ExtendedDouble
+    {
+        #region 定数
+        /// <summary>
+        /// 符号部のビット数
+        /// </summary>
+        public const int SignLength = 1;
+        /// <summary>
+        /// 指数部のビット数
+        /// </summary>
+        public const int ExponentLength = 11;
+        /// <summary>
+        /// 指数部のビットマスク
+        /// </summary>
+        public const UInt ExponentBitMask = ((UInt)1 << ExponentLength) - 1;
+        /// <summary>
+        /// 指数部バイアス
+        /// </summary>
+        public const int ExponentBias = 1023;
+        /// <summary>
+        /// 無限大を表す指数部
+        /// </summary>
+        public const int InfinityExponent = 1024;
+        /// <summary>
+        /// 仮数部のビット数
+        /// </summary>
+        public const int MantissaLength = 52;
+        /// <summary>
+        /// 仮数部のビットマスク
+        /// </summary>
+        public const UInt MantissaBitMask = ((UInt)1 << MantissaLength) - 1;
+        #endregion 定数
+
+        #region フィールド
+        [FieldOffset(0)]
+        public Double Value;
+        [FieldOffset(0)]
+        public UInt Bits;
+        #endregion フィールド
+
+        #region プロパティ
+        /// <summary>
+        /// 符号ビットを取得
+        /// </summary>
+        public byte SignBit => (byte)(Bits >> (ExponentLength + MantissaLength));
+        /// <summary>
+        /// 符号を取得
+        /// </summary>
+        public int Sign => SignBit == 1 ? -1 : +1;
+
+        /// <summary>
+        /// 指数部を取得
+        /// </summary>
+        public ushort ExponentBits => (ushort)((Bits >> MantissaLength) & ExponentBitMask);
+        /// <summary>
+        /// 指数を取得
+        /// </summary>
+        public int Exponent => ExponentBits - ExponentBias;
+
+        /// <summary>
+        /// 仮数部を取得
+        /// </summary>
+        public UInt MantissaBits => Bits & MantissaBitMask;
+        /// <summary>
+        /// 仮数を取得
+        /// </summary>
+        public UInt Mantissa => MantissaBits | ((UInt)1 << MantissaLength);// ((UInt)1 << MantissaLength)は"1."を意味する
+        #endregion プロパティ
+    }
+}
---
+++ Ksnm/KsnmTests/Numerics/ExtendedDoubleTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ksnm.Numerics.Tests
+{
+    [TestClass()]
+    public class ExtendedDoubleTests
+    {
+        [TestMethod()]
+        public void PropertyTest()
+        {
+            // 1.00
+            {
+                ExtendedDouble extendedDouble = new ExtendedDouble();
+                extendedDouble.Value = 1.0;
+                Assert.AreEqual(0b0_01111111111_0000000000000000000000000000000000000000000000000000ul, extendedDouble.Bits);
+                Assert.AreEqual(0, extendedDouble.SignBit);
+                Assert.AreEqual(+1, extendedDouble.Sign);
+                Assert.AreEqual(0b01111111111, extendedDouble.ExponentBits);
+                Assert.AreEqual(0, extendedDouble.Exponent);
+                Assert.AreEqual(0b0000000000000000000000000000000000000000000000000000ul, extendedDouble.MantissaBits);
+                Assert.AreEqual(0b10000000000000000000000000000000000000000000000000000ul, extendedDouble.Mantissa);
+            }
+            // 1.00
+            {
+                ExtendedDouble extendedDouble = new ExtendedDouble();
+                extendedDouble.Value = -1.0;
+                Assert.AreEqual(0b1_01111111111_0000000000000000000000000000000000000000000000000000ul, extendedDouble.Bits);
+                Assert.AreEqual(1, extendedDouble.SignBit);
+                Assert.AreEqual(-1, extendedDouble.Sign);
+                Assert.AreEqual(0b01111111111, extendedDouble.ExponentBits);
+                Assert.AreEqual(0, extendedDouble.Exponent);
+                Assert.AreEqual(0b0000000000000000000000000000000000000000000000000000ul, extendedDouble.MantissaBits);
+                Assert.AreEqual(0b10000000000000000000000000000000000000000000000000000ul, extendedDouble.Mantissa);
+            }
+            // 1.23
+            {
+                ExtendedDouble extendedDouble = new ExtendedDouble();
+                extendedDouble.Value = 1.23;
+                Assert.AreEqual(0b0_01111111111_0011101011100001010001111010111000010100011110101110ul, extendedDouble.Bits);
+                Assert.AreEqual(0, extendedDouble.SignBit);
+                Assert.AreEqual(+1, extendedDouble.Sign);
+                Assert.AreEqual(0b01111111111, extendedDouble.ExponentBits);
+                Assert.AreEqual(0, extendedDouble.Exponent);
+                Assert.AreEqual(0b0011101011100001010001111010111000010100011110101110ul, extendedDouble.MantissaBits);
+                Assert.AreEqual(0b10011101011100001010001111010111000010100011110101110ul, extendedDouble.Mantissa);
+            }
+            // 2.00
+            {
+                ExtendedDouble extendedDouble = new ExtendedDouble();
+                extendedDouble.Value = 2.0;
+                Assert.AreEqual(0b0_10000000000_0000000000000000000000000000000000000000000000000000ul, extendedDouble.Bits);
+                Assert.AreEqual(0, extendedDouble.SignBit);
+                Assert.AreEqual(+1, extendedDouble.Sign);
+                Assert.AreEqual(0b10000000000, extendedDouble.ExponentBits);
+                Assert.AreEqual(1, extendedDouble.Exponent);
+                Assert.AreEqual(0b0000000000000000000000000000000000000000000000000000ul, extendedDouble.MantissaBits);
+                Assert.AreEqual(0b10000000000000000000000000000000000000000000000000000ul, extendedDouble.Mantissa);
+            }
+        }
+    }
+}
