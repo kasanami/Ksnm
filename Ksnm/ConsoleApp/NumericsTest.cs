77766f11e77937de1510c535ecbe67ce0aa0f7be
笠浪貴大 <dev.kacha@gmail.com>
2024-09-24 23:23:20 +0900

    各種数値型のテスト追加

--- Ksnm/ConsoleApp/NumericsTest.cs
+++ Ksnm/ConsoleApp/NumericsTest.cs
@@ -16,50 +16,12 @@ namespace ConsoleApp
         {
             Console.WriteLine(Ksnm.Debug.GetFilePathAndLineNumber());
 
-            Console.WriteLine($"ExtendedDouble");
-            for (double i = -10; i <= 10; i += 0.5)
-            {
-                ExtendedDouble d = i;
-                Console.WriteLine($"Value   :{d.Value}");
-                Console.WriteLine($"Bits    :{d.Bits:X}");
-                Console.WriteLine($"Sign    :{d.Sign}");
-                Console.WriteLine($"Mantissa:{d.Mantissa}");
-                Console.WriteLine($"Exponent:{d.Exponent}");
-                Console.WriteLine($"Scale   :{d.Scale}");
-                var value = d.Mantissa * d.Scale * d.Sign;
-                Console.WriteLine($"{value}");
-                Console.WriteLine();
-            }
+            ExtendedDoubleTest();
+            ExtendedDecimalTest();
+            CubedDividedNumber8Test();
+            FractionTest();
 
-            Console.WriteLine($"ExtendedDecimal");
-            for (decimal i = -10; i <= 10; i += 0.5m)
-            {
-                ExtendedDecimal d = i;
-                Console.WriteLine($"Value   :{d.Value}");
-                Console.WriteLine($"Bits    :{d.Bits:X}");
-                Console.WriteLine($"Sign    :{d.Sign}");
-                Console.WriteLine($"Mantissa:{d.Mantissa}");
-                Console.WriteLine($"Exponent:{d.Exponent}");
-                Console.WriteLine($"Scale   :{d.Scale}");
-                var value = (double)d.Mantissa * d.Scale * d.Sign;
-                Console.WriteLine($"{value}");
-                Console.WriteLine();
-            }
 
-            Console.WriteLine($"double → Fraction");
-            for (double i = -10; i <= 10; i++)
-            {
-                Fraction<long> fraction = new Fraction<long>(i);
-                fraction.Reduce();
-                Console.WriteLine($"{i} → {fraction}");
-            }
-            Console.WriteLine($"decimal → Fraction");
-            for (decimal i = -10; i <= 10; i++)
-            {
-                Fraction<long> fraction = new Fraction<long>(i);
-                fraction.Reduce();
-                Console.WriteLine($"{i} → {fraction}");
-            }
             // NaNはキャスト可能？→可能
             if (false)
             {
@@ -76,22 +38,50 @@ namespace ConsoleApp
                 f64 = (double)f32;
                 Console.WriteLine($"{f64} {BitConverter.DoubleToUInt64Bits(f64):X16}");
             }
-            // 数学定数を計算
-            if (false)
+        }
+
+        public static void ExtendedDoubleTest()
+        {
+            Console.WriteLine(Ksnm.Debug.GetFilePathAndLineNumber());
+
+            Console.WriteLine($"ExtendedDouble");
+            for (double i = -10; i <= 10; i += 0.5)
             {
-                var exp = (double)Ksnm.Math.CalculateE<Fraction32>(Fraction32.Epsilon, 5);
-                var pi = (double)Ksnm.Math.CalculatePi<Fraction32>();
+                ExtendedDouble d = i;
+                Console.WriteLine($"Value   :{d.Value}");
+                Console.WriteLine($"Bits    :{d.Bits:X}");
+                Console.WriteLine($"Sign    :{d.Sign}");
+                Console.WriteLine($"Mantissa:{d.Mantissa}");
+                Console.WriteLine($"Exponent:{d.Exponent}");
+                Console.WriteLine($"Scale   :{d.Scale}");
+                var value = d.Mantissa * d.Scale * d.Sign;
+                Console.WriteLine($"{value}");
+                Console.WriteLine();
             }
 
+            Console.WriteLine();
+        }
+
+        public static void ExtendedDecimalTest()
+        {
+            Console.WriteLine(Ksnm.Debug.GetFilePathAndLineNumber());
+
+            Console.WriteLine($"ExtendedDecimal");
+            for (decimal i = -10; i <= 10; i += 0.5m)
             {
-                var bigFraction = new BigFraction(1, 2);// 1/2
-                Console.WriteLine(bigFraction.ToString());
+                ExtendedDecimal d = i;
+                Console.WriteLine($"Value   :{d.Value}");
+                Console.WriteLine($"Bits    :{d.Bits:X}");
+                Console.WriteLine($"Sign    :{d.Sign}");
+                Console.WriteLine($"Mantissa:{d.Mantissa}");
+                Console.WriteLine($"Exponent:{d.Exponent}");
+                Console.WriteLine($"Scale   :{d.Scale}");
+                var value = (double)d.Mantissa * d.Scale * d.Sign;
+                Console.WriteLine($"{value}");
+                Console.WriteLine();
             }
 
-            {
-                var bigFraction = new BigFraction(1, 3);// 1/3
-                Console.WriteLine(bigFraction.ToString());
-            }
+            Console.WriteLine();
         }
 
         public static void CubedDividedNumber8Test()
@@ -121,6 +111,47 @@ namespace ConsoleApp
                 cdn = (CubedDividedNumber8)d;
                 Console.WriteLine($"{d}→{cdn.Value}→{(double)cdn}");
             }
+
+            Console.WriteLine();
+        }
+
+        public static void FractionTest()
+        {
+            Console.WriteLine(Ksnm.Debug.GetFilePathAndLineNumber());
+
+
+            Console.WriteLine($"double → Fraction");
+            for (double i = -10; i <= 10; i++)
+            {
+                Fraction<long> fraction = new Fraction<long>(i);
+                fraction.Reduce();
+                Console.WriteLine($"{i} → {fraction}");
+            }
+            Console.WriteLine($"decimal → Fraction");
+            for (decimal i = -10; i <= 10; i++)
+            {
+                Fraction<long> fraction = new Fraction<long>(i);
+                fraction.Reduce();
+                Console.WriteLine($"{i} → {fraction}");
+            }
+            // 数学定数を計算
+            if (false)
+            {
+                var exp = (double)Ksnm.Math.CalculateE<Fraction32>(Fraction32.Epsilon, 5);
+                var pi = (double)Ksnm.Math.CalculatePi<Fraction32>();
+            }
+
+            {
+                var bigFraction = new BigFraction(1, 2);// 1/2
+                Console.WriteLine(bigFraction.ToString());
+            }
+
+            {
+                var bigFraction = new BigFraction(1, 3);// 1/3
+                Console.WriteLine(bigFraction.ToString());
+            }
+
+            Console.WriteLine();
         }
     }
 }
