70218240f3d6dc11b7115868871118e44c73f877
笠浪貴大 <dev.kacha@gmail.com>
2024-11-03 23:10:47 +0900

    HammingDistance() 追加

---
+++ Ksnm/Ksnm/Text/String.cs
@@ -0,0 +1,23 @@
+﻿namespace Ksnm.Text
+{
+    public static class String
+    {
+        /// <summary>
+        /// 2つの文字列のハミング距離を求める
+        /// </summary>
+        public static int HammingDistance(string text1, string text2)
+        {
+            var maxLength = Math.Max(text1.Length, text2.Length);
+            var minLength = Math.Min(text1.Length, text2.Length);
+            int distance = maxLength - minLength;
+            for (int i = 0; i < minLength; i++)
+            {
+                if (text1[i] != text2[i])
+                {
+                    distance++;
+                }
+            }
+            return distance;
+        }
+    }
+}
