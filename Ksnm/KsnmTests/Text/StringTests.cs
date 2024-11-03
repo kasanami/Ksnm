55e146e1e29f36968ab43872caf3f977db969f50
笠浪貴大 <dev.kacha@gmail.com>
2024-11-03 23:21:47 +0900

    単体テスト追加

---
+++ Ksnm/KsnmTests/Text/StringTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Ksnm.Text.Tests
+{
+    [TestClass()]
+    public class StringTests
+    {
+        [TestMethod()]
+        public void HammingDistanceTest()
+        {
+            Assert.AreEqual(0, String.HammingDistance("", ""));
+            Assert.AreEqual(0, String.HammingDistance("1", "1"));
+            Assert.AreEqual(0, String.HammingDistance("10", "10"));
+
+            Assert.AreEqual(1, String.HammingDistance("0", ""));
+            Assert.AreEqual(1, String.HammingDistance("1", ""));
+            Assert.AreEqual(1, String.HammingDistance("0", "1"));
+            Assert.AreEqual(1, String.HammingDistance("1", "0"));
+            Assert.AreEqual(1, String.HammingDistance("00", "01"));
+            Assert.AreEqual(1, String.HammingDistance("00", "10"));
+
+            Assert.AreEqual(2, String.HammingDistance("00", ""));
+            Assert.AreEqual(2, String.HammingDistance("11", ""));
+            Assert.AreEqual(2, String.HammingDistance("00", "11"));
+            Assert.AreEqual(2, String.HammingDistance("11", "00"));
+        }
+    }
+}
