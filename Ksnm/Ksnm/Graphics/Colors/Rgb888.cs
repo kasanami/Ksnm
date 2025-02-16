a554926b6b81c3638a4a54e9781286d926f1fc46
笠浪貴大 <dev.kacha@gmail.com>
2025-02-16 16:42:47 +0900

    演算子追加

--- Ksnm/Ksnm/Graphics/Colors/Rgb888.cs
+++ Ksnm/Ksnm/Graphics/Colors/Rgb888.cs
@@ -1,14 +1,17 @@
 ﻿using Ksnm.Numerics;
+using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace Ksnm.Graphics.Colors
 {
     /// <summary>
     /// 色
     /// </summary>
-    /// <param name="R">赤色</param>
-    /// <param name="G">緑色</param>
-    /// <param name="B">青色</param>
-    public record struct Rgb888(ScaledNumber8 R, ScaledNumber8 G, ScaledNumber8 B)
+    [StructLayout(LayoutKind.Explicit)]
+    public struct Rgb888 :
+        IEquatable<Rgb888>,
+        IAdditionOperators<Rgb888, Rgb888, Rgb888>,
+        ISubtractionOperators<Rgb888, Rgb888, Rgb888>
     {
         #region 定数,static
         public static readonly Rgb888 Red = new(1.0, 0.0, 0.0);
@@ -16,9 +19,24 @@ namespace Ksnm.Graphics.Colors
         public static readonly Rgb888 Blue = new(0.0, 0.0, 1.0);
         #endregion 定数,static
 
+        #region フィールド,プロパティ
+        [FieldOffset(0)]
+        public ScaledNumber8 R;
+        [FieldOffset(1)]
+        public ScaledNumber8 G;
+        [FieldOffset(2)]
+        public ScaledNumber8 B;
+        #endregion フィールド,プロパティ
+
         #region コンストラクタ
         public Rgb888() : this(ScaledNumber8.Zero, ScaledNumber8.Zero, ScaledNumber8.Zero)
         {
+        }
+        public Rgb888(ScaledNumber8 r, ScaledNumber8 g, ScaledNumber8 b)
+        {
+            R = r;
+            G = g;
+            B = b;
         }
         public Rgb888(double r, double g, double b) :
             this((ScaledNumber8)Math.Clamp(r, 0, 1), (ScaledNumber8)Math.Clamp(g, 0, 1), (ScaledNumber8)Math.Clamp(b, 0, 1))
@@ -26,9 +44,46 @@ namespace Ksnm.Graphics.Colors
         }
         #endregion コンストラクタ
 
+        #region IEquatable
+        public override bool Equals(object? obj)
+        {
+            return obj is Rgb888 rgb && Equals(rgb);
+        }
+
+        public bool Equals(Rgb888 other)
+        {
+            return R.Equals(other.R) && G.Equals(other.G) && B.Equals(other.B);
+        }
+        #endregion IEquatable
+
         public RgbFFF ToRgbFFF()
         {
             return new((float)R, (float)G, (float)B);
         }
+
+        #region operator
+        public static bool operator ==(Rgb888 left, Rgb888 right)
+        {
+            return left.Equals(right);
+        }
+        public static bool operator !=(Rgb888 left, Rgb888 right)
+        {
+            return !(left == right);
+        }
+        public static Rgb888 operator +(Rgb888 left, Rgb888 right)
+        {
+            return new(
+                left.R + right.R,
+                left.G + right.G,
+                left.B + right.B);
+        }
+        public static Rgb888 operator -(Rgb888 left, Rgb888 right)
+        {
+            return new(
+                left.R - right.R,
+                left.G - right.G,
+                left.B - right.B);
+        }
+        #endregion operator
     }
 }
--- Ksnm/KsnmTests/Graphics/Colors/Rgb888Tests.cs
+++ Ksnm/KsnmTests/Graphics/Colors/Rgb888Tests.cs
@@ -6,6 +6,7 @@
         [TestMethod()]
         public void OperationsTest()
         {
+            // ==
             {
                 Rgb888 rgb1 = Rgb888.Red;
                 Rgb888 rgb2 = Rgb888.Red;
@@ -20,6 +21,40 @@
                 Assert.IsFalse(rgb1 == rgb2);
                 rgb2 = new(0, 1, 0);
                 Assert.IsFalse(rgb1 == rgb2);
+                rgb2 = new(0, 0, 1);
+                Assert.IsFalse(rgb1 == rgb2);
+            }
+            // !=
+            {
+                Rgb888 rgb1 = Rgb888.Red;
+                Rgb888 rgb2 = Rgb888.Red;
+                Assert.IsFalse(rgb1 != rgb2);
+                rgb1 = new(1, 1, 1);
+                rgb2 = new(1, 1, 1);
+                Assert.IsFalse(rgb1 != rgb2);
+                rgb1 = new(0, 0, 0);
+                rgb2 = new(0, 0, 0);
+                Assert.IsFalse(rgb1 != rgb2);
+                rgb2 = new(1, 0, 0);
+                Assert.IsTrue(rgb1 != rgb2);
+                rgb2 = new(0, 1, 0);
+                Assert.IsTrue(rgb1 != rgb2);
+                rgb2 = new(0, 0, 1);
+                Assert.IsTrue(rgb1 != rgb2);
+            }
+            // +
+            {
+                Rgb888 rgb1 = new(0.2, 0.4, 0.6);
+                Rgb888 rgb2 = new(0.2, 0.2, 0.2);
+                Rgb888 rgb3 = rgb1 + rgb2;
+                Assert.AreEqual(new(0.4, 0.6, 0.8), rgb3);
+            }
+            // -
+            {
+                Rgb888 rgb1 = new(0.2, 0.4, 0.6);
+                Rgb888 rgb2 = new(0.2, 0.2, 0.2);
+                Rgb888 rgb3 = rgb1 - rgb2;
+                Assert.AreEqual(new(0.0, 0.2, 0.4), rgb3);
             }
         }
         [TestMethod()]
