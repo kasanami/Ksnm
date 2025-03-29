e0cf071e5585eb002030427bdb278ec71be1f1cc
笠浪貴大 <dev.kacha@gmail.com>
2025-03-30 06:32:26 +0900

    StringConcatMesurement追加

---
+++ Ksnm/ConsoleApp/StringConcatMesurement.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+
+namespace ConsoleApp
+{
+    /// <summary>
+    /// 文字列を接続する速度を測定
+    /// （BenchmarkDotNetのサンプル）
+    /// </summary>
+    [MemoryDiagnoser]
+    [MinColumn, MaxColumn]
+    public class StringConcatMesurement
+    {
+        private int NumberOfItems = 2000;
+        [Benchmark]
+        public string WithStringBuilder()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < NumberOfItems; i++)
+            {
+                sb.Append("1");
+            }
+            return sb.ToString();
+        }
+        [Benchmark]
+        public string WithStringType()
+        {
+            string s = string.Empty;
+            for (int i = 0; i < NumberOfItems; i++)
+            {
+                s += "1";
+            }
+            return s;
+        }
+    }
+}
